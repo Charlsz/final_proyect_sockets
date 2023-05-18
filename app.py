8796b7e85c63e51f531c64bb800e8602c8f95071
Elturco23 <jriano@uninorte.edu.co>
2023-05-18 10:26:46 -0500

    formulario para tamaño del vector

--- app.py
+++ app.py
@@ -1,10 +1,18 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def home():
+    if request.method == 'POST':
+        # Obtener el tamaño del vector enviado por el formulario
+        vector_size = int(request.form['vector-size'])
+        
+        # Realizar el procesamiento y ordenación del vector aquí
+        
+        return render_template('resultado.html')  # Renderizar el template de resultado
+        
     return render_template('index.html')
-
 
 if __name__== '__main__':
     app.run(debug=True)#Cambiar luego
