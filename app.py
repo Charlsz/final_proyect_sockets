e931baa0632eae9a2ab5da71fa887f6b21fb0b9d
Elturco23 <jriano@uninorte.edu.co>
2023-05-18 10:36:33 -0500

    vector generado en base al form

--- app.py
+++ app.py
@@ -1,19 +1,16 @@
 from flask import Flask, render_template, request
-
+import random
 app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
     if request.method == 'POST':
-        # Obtener el tamaño del vector enviado por el formulario
         vector_size = int(request.form['vector-size'])
-        
-        # Realizar el procesamiento y ordenación del vector aquí
-        
-        return render_template('resultado.html')  # Renderizar el template de resultado
+        vector = [random.randint(1, 1000000) for _ in range(vector_size)]
+        n = vector
+        return render_template('index.html', vector=vector)
         
     return render_template('index.html')
-
 if __name__== '__main__':
     app.run(debug=True)#Cambiar luego
 
